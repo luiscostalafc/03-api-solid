8deb34072f9aa8628e47018fb667c0daabcdcada
luiscostalafc <luiscostalafc@gmail.com>
2024-05-05 15:00:18 -0300

    feat: create get-user-profile use case

---
+++ src/use-cases/get-user-profile.ts
@@ -0,0 +1,30 @@
+import { User } from "@prisma/client";
+
+import { UsersRepository } from "@/repositories/users-repository";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+
+interface GetUserProfileUseCaseRequest {
+  userId: string;
+}
+
+interface GetUserProfileUseCaseRespose {
+  user: User;
+}
+
+export class GetUserProfileUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    userId,
+  }: GetUserProfileUseCaseRequest): Promise<GetUserProfileUseCaseRespose> {
+    const user = await this.usersRepository.findById(userId);
+
+    if (!user) {
+      throw new ResourceNotFoundError();
+    }
+
+    return {
+      user,
+    };
+  }
+}
