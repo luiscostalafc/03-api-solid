e43912a6592c89f0c807bad4ad2204d0b3dad38b
luiscostalafc <luiscostalafc@gmail.com>
2024-05-20 08:46:56 -0300

    feat: create authenticate util

--- src/http/controllers/users/profile.spec.ts
+++ src/http/controllers/users/profile.spec.ts
@@ -2,6 +2,7 @@ import { describe, it, beforeAll, afterAll, expect } from "vitest";
 import request from "supertest";
 
 import { app } from "@/app";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
 
 describe("Profile (e2e)", () => {
   beforeAll(async () => {
@@ -13,18 +14,7 @@ describe("Profile (e2e)", () => {
   });
 
   it("should be able to get user profile", async () => {
-    await request(app.server).post("/users").send({
-      name: "John Doe",
-      email: "johdoe@example.com",
-      password: "123456",
-    });
-
-    const authResponse = await request(app.server).post("/sessions").send({
-      email: "johdoe@example.com",
-      password: "123456",
-    });
-
-    const { token } = authResponse.body;
+    const { token } = await createAndAuthenticateUser(app);
 
     const profileResponse = await request(app.server)
       .get("/me")
---
+++ src/utils/test/create-and-authenticate-user.ts
@@ -0,0 +1,22 @@
+import { FastifyInstance } from "fastify";
+
+import request from "supertest";
+
+export async function createAndAuthenticateUser(app: FastifyInstance) {
+  await request(app.server).post("/users").send({
+    name: "John Doe",
+    email: "johdoe@example.com",
+    password: "123456",
+  });
+
+  const authResponse = await request(app.server).post("/sessions").send({
+    email: "johdoe@example.com",
+    password: "123456",
+  });
+
+  const { token } = authResponse.body;
+
+  return {
+    token,
+  };
+}
