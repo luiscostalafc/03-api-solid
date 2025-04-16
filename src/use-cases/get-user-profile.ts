119858203f8182d46520ee227303c895267aa64b
Luis Ferreira da Costa <luis.costa@multivision.pt>
2025-04-16 15:26:54 +0100

    fix: updates in check-in file

--- src/use-cases/get-user-profile.ts
+++ src/use-cases/get-user-profile.ts
@@ -13,6 +13,7 @@ interface GetUserProfileUseCaseResponse {
 
 export class GetUserProfileUseCase {
   constructor(private usersRepository: UsersRepository) {}
+  // Test diff
   async execute({
     userId,
   }: GetUserProfileUseCaseRequest): Promise<GetUserProfileUseCaseResponse> {
